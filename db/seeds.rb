d9bd6938b0cd902a17447a643d19ef4ac6d15315
Marie Altieri <marie.altieri@hotmail.fr>
2022-11-17 09:11:35 +0100

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+20.times do
+  category = ["chinese", "italian", "japanese", "french", "belgian"].sample
+  Restaurant.create(name: Faker::Restaurant.name, address: Faker::Beer.yeast, phone_number:"00 32 471 23 44 11", category: category)
+end
+
